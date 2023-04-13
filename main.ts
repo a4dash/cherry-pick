12d1eb296a56a021419f87af5aa374b292a51267
a4dash <130414964+a4dash@users.noreply.github.com>
2023-04-13 21:27:54 +1000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+game.splash("Cherry Picker")
+tiles.setCurrentTilemap(tilemap`level2`)
+let mySprite = sprites.create(img`
+    . . . . . f f f f f . . . . . . 
+    . . . . f e e e e e f . . . . . 
+    . . . f d d d d d d e f . . . . 
+    . . f d f f d d f f d f f . . . 
+    . c d d d e e d d d d e d f . . 
+    . c d c d d d d c d d e f f . . 
+    . c d d c c c c d d d e f f f f 
+    . . c d d d d d d d e f f b d f 
+    . . . c d d d d e e f f f d d f 
+    . . . . f f f e e f e e e f f f 
+    . . . . f e e e e e e e f f f . 
+    . . . f e e e e e e f f f e f . 
+    . . f f e e e e f f f f f e f . 
+    . f b d f e e f b b f f f e f . 
+    . f d d f f f f d d b f f f f . 
+    . f f f f f f f f f f f f f . . 
+    `, SpriteKind.Player)
+controller.moveSprite(mySprite, 150, 150)
+scene.cameraFollowSprite(mySprite)
